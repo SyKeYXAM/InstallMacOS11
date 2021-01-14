c296ad2124a22d9013d1b0036fe15f3e466527fd
SyKeY XAM <46406915+SyKeYXAM@users.noreply.github.com>
2021-01-14 16:34:58 +0100

    Update CrearDisco.sh

--- CrearDisco.sh
+++ CrearDisco.sh
@@ -2,8 +2,8 @@
 
 #Definir disco de instalación para MacOS 11 BigSur
 #export Disco=$PWD/DiskBigSur.qcow2 #/media/sykey/538042f0-8415-4720-9850-47245ef68113/home/sykey/Escritorio/DiscoBigSur/DiskBigSur.qcow2
-export Disco=/media/sykey/Windows-SSD/Users/sykey/Desktop/BigSur/MyDisk.qcow2
-export Capacidad=64G
+Disco=/media/sykey/Windows-SSD/Users/sykey/Desktop/BigSur/MyDisk.qcow2
+Capacidad=64G
 
 #Creamos el tamaño del disco duro
 qemu-img create -f qcow2 $Disco $Capacidad
